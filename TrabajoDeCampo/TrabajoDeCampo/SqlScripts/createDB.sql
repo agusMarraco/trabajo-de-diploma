dbc2ade68d28617f3bc74dfaa35af02e9e8889a9
Agustin <adm101195@gmail.com>
2018-09-19 21:34:22 -0300

    --cambios de scripts

--- TrabajoDeCampo/TrabajoDeCampo/SqlScripts/createDB.sql
+++ TrabajoDeCampo/TrabajoDeCampo/SqlScripts/createDB.sql
@@ -255,3 +255,11 @@ PAT.PAT_ID = UP.UP_PATENTE_ID)
 WHERE 
 USU.USU_BAJA <> 1
 
+-- vista bitacora
+
+go
+
+  create or alter view vistaBitacora as 
+  select bi.BIT_FECHA,usu.USU_ALIAS,bi.BIT_CRITICIDAD_ID, bi.BIT_MENSAJE from BITACORA bi
+  inner join usuario usu on usu.USU_ID = bi.BIT_USUARIO
+  inner join CRITICIDAD crit on crit.CRIT_ID = bi.BIT_CRITICIDAD_ID
